b4195ee5d4553f79a5c51d5685abb37f60cb762c
partho0 <68466005+partho0@users.noreply.github.com>
2022-08-17 02:13:09 +0600

    header

--- home.php
+++ home.php
@@ -22,7 +22,16 @@
    <!-- head starts-->
 
    <section class="header">
-      <a href="home.php"></a>
+      <a href="home.php" class="logo">Travel</a>
+
+      <nav class="navbar">
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="package.php">Package</a>
+         <a href="book.php">Book</a>
+      </nav>
+
+      <div id="menu-btn" class="fas fa-bars"></div>
    </section>
 
    <!--head end-->
