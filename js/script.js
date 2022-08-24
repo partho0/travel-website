e17665cb677340b6893572d837954f4a47eadbfa
partho0 <68466005+partho0@users.noreply.github.com>
2022-08-24 16:37:21 +0600

    header

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .navbar');
 
 menu.onclick = () =>{
-   menu.classList.toggle('fa-times');
-   navbar.classList.toggle('active');
+menu.classList.toggle('fa-times');
+navbar.classList.toggle('active');
 }
