5f3a867cdfe3a27cecd7976cd36e30cbae1cb4a7
partho0 <68466005+partho0@users.noreply.github.com>
2022-08-25 14:32:53 +0600

    services

---
+++ package.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Home</title>
+
+   <!-- font awesome cdn link -->
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+
+   <!-- Swiper Css -->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+
+
+   <!-- custom css link -->
+
+   <link rel="stylesheet" href=".//css/style.css">
+
+</head>
+
+<body>
+   <!-- header starts -->
+   <section class="header">
+      <a href="home.php" class="logo">Travel.</a>
+
+      <nav class="navbar">
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="package.php">Package</a>
+         <a href="book.php">Book</a>
+
+      </nav>
+
+      <div id="menu-btn" class="fas fa-bars"></div>
+
+   </section>
+
+   <!-- header ends -->
+
+   <div class="heading" style="background: url(images/header-bg-2.png);">
+      <h1>packages</h1>
+   </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <!-- footer section starts -->
+
+   <section class="footer">
+      <div class="box-container">
+         <div class="box">
+            <h3>
+               quick links
+            </h3>
+            <a href="home.php"><i class="fas fa-angle-right">Home</i></a>
+            <a href="about.php"><i class="fas fa-angle-right"></i>About</a>
+            <a href="package.php"><i class="fas fa-angle-right"></i>Package</a>
+            <a href="book.php"><i class="fas fa-angle-right"></i>Book</a>
+         </div>
+
+         <div class="box">
+            <h3>
+               extra links
+            </h3>
+            <a href="#"><i class="fas fa-angle-right">ask question</i></a>
+            <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+            <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+            <a href="#"><i class="fas fa-angle-right"></i>terms of condition</a>
+         </div>
+
+         <div class="box">
+            <h3>
+               contact info
+            </h3>
+            <a href="#"><i class="fas fa-envelope"></i>yeasin.udoy89@gmail.com</a>
+            <a href="#"><i class="fas fa-map"></i>Dhaka, Bangladesh</a>
+         </div>
+
+         <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fab fa-facebook"></i>facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+
+         </div>
+
+      </div>
+
+      <div class="credit">
+         created by <span>Yeasin Udoy</span> || all rights reserved
+      </div>
+
+   </section>
+
+   <!-- footer section ends -->
+
+
+
+   <!-- Swiper Js -->
+   <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+
+   <!-- custom js file link -->
+   <script src="./js/script.js"></script>
+
+
+</body>
+
+</html>
